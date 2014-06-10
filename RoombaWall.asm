36cf3f4e15601137a03676f1c892f6c59176ed29
Alvaro G. M <alvaro.gamez@hazent.com>
2014-06-10 19:21:12 +0200

    tabs

--- RoombaWall.asm
+++ RoombaWall.asm
@@ -1,64 +1,64 @@
-    LIST p=12lf1571
-    #include p12lf1571.inc
+	LIST p=12lf1571
+	#include p12lf1571.inc
 
-    ; CONFIG1
-    ; __config 0xFF84
-     __CONFIG _CONFIG1, _FOSC_INTOSC & _WDTE_OFF & _PWRTE_ON & _MCLRE_OFF & _CP_OFF & _BOREN_ON & _CLKOUTEN_OFF
-    ; CONFIG2
-    ; __config 0xFEFF
-     __CONFIG _CONFIG2, _WRT_OFF & _PLLEN_OFF & _STVREN_ON & _BORV_LO & _LPBOREN_OFF & _LVP_ON
+	; CONFIG1
+	; __config 0xFF84
+	 __CONFIG _CONFIG1, _FOSC_INTOSC & _WDTE_OFF & _PWRTE_ON & _MCLRE_OFF & _CP_OFF & _BOREN_ON & _CLKOUTEN_OFF
+	; CONFIG2
+	; __config 0xFEFF
+	 __CONFIG _CONFIG2, _WRT_OFF & _PLLEN_OFF & _STVREN_ON & _BORV_LO & _LPBOREN_OFF & _LVP_ON
 
 
-    org 0x0000
-    goto SetUp
+	org 0x0000
+	goto SetUp
 
-    cblock 0x20
-        delayCounter
-    endc
+	cblock 0x20
+		delayCounter
+	endc
 
 #define CMD_WALL B'10100010'
 
 SetUp
-    ; Configure clock
-    movlw B'00111011'
-    banksel OSCCON
-    movwf OSCCON
+	; Configure clock
+	movlw B'00111011'
+	banksel OSCCON
+	movwf OSCCON
 
-    ; Porta A as digital output
-    banksel TRISA
-    clrf TRISA
+	; Porta A as digital output
+	banksel TRISA
+	clrf TRISA
 
-    ; Disable interrupts
-    clrf PWM3IE
+	; Disable interrupts
+	clrf PWM3IE
 
-    ; Prescale 128
-    movlw B'00000000'
-    banksel PWM3CLKCON
-    movwf PWM3CLKCON
+	; Prescale 128
+	movlw B'00000000'
+	banksel PWM3CLKCON
+	movwf PWM3CLKCON
 
-    ; PR = 12 = - 12
-    movlw B'00000000'
-    banksel PWM3PRH
-    movwf PWM3PRH
-    movlw D'12'
-    banksel PWM3PRL
-    movwf PWM3PRL
+	; PR = 12 = - 12
+	movlw B'00000000'
+	banksel PWM3PRH
+	movwf PWM3PRH
+	movlw D'12'
+	banksel PWM3PRL
+	movwf PWM3PRL
 
-    ; DC = 6 = - 6
-    movlw B'00000000'
-    banksel PWM3DCH
-    movwf PWM3DCH
-    movlw D'6'
-    banksel PWM3DCL
-    movwf PWM3DCL
+	; DC = 6 = - 6
+	movlw B'00000000'
+	banksel PWM3DCH
+	movwf PWM3DCH
+	movlw D'6'
+	banksel PWM3DCL
+	movwf PWM3DCL
 
-    clrf PWM3PHH
-    clrf PWM3PHL
+	clrf PWM3PHH
+	clrf PWM3PHL
 
 ; Enables PWM output pin
-    movlw B'11100000'
-    banksel PWM3CON
-    movwf PWM3CON
+	movlw B'11100000'
+	banksel PWM3CON
+	movwf PWM3CON
 
 	BANKSEL PORTA ;
 	CLRF PORTA ;Init PORTA
@@ -68,48 +68,48 @@ SetUp
 	CLRF ANSELA ;digital I/O
 	BANKSEL PORTA ;
 MainLoop
-    ; This is everything but scalable:
-    bcf PORTA, 4
-    call delay3ms
-    bsf PORTA, 4
-    call delay1ms
+	; This is everything but scalable:
+	bcf PORTA, 4
+	call delay3ms
+	bsf PORTA, 4
+	call delay1ms
 
-    bcf PORTA, 4
-    call delay1ms
-    bsf PORTA, 4
-    call delay3ms
+	bcf PORTA, 4
+	call delay1ms
+	bsf PORTA, 4
+	call delay3ms
 
-    bcf PORTA, 4
-    call delay3ms
-    bsf PORTA, 4
-    call delay1ms
+	bcf PORTA, 4
+	call delay3ms
+	bsf PORTA, 4
+	call delay1ms
 
-    bcf PORTA, 4
-    call delay1ms
-    bsf PORTA, 4
-    call delay3ms
+	bcf PORTA, 4
+	call delay1ms
+	bsf PORTA, 4
+	call delay3ms
 
-    bcf PORTA, 4
-    call delay1ms
-    bsf PORTA, 4
-    call delay3ms
+	bcf PORTA, 4
+	call delay1ms
+	bsf PORTA, 4
+	call delay3ms
 
-    bcf PORTA, 4
-    call delay1ms
-    bsf PORTA, 4
-    call delay3ms
+	bcf PORTA, 4
+	call delay1ms
+	bsf PORTA, 4
+	call delay3ms
 
-    bcf PORTA, 4
-    call delay3ms
-    bsf PORTA, 4
-    call delay1ms
+	bcf PORTA, 4
+	call delay3ms
+	bsf PORTA, 4
+	call delay1ms
 
-    bcf PORTA, 4
-    call delay1ms
-    bsf PORTA, 4
-    call delay3ms
+	bcf PORTA, 4
+	call delay1ms
+	bsf PORTA, 4
+	call delay3ms
 
-    goto MainLoop
+	goto MainLoop
 
 
 
@@ -122,20 +122,20 @@ MainLoop
 ; 164*3 = 492
 ; Plus 6 fixed cycles, 498.
 ; I only need to place now 2 extra cycles
-delay1ms                    ; 2
-    goto $+1                ; 2 These are the two extra cycles above
-    movlw D'164'            ; 1
-    movwf delayCounter      ; 1
+delay1ms					; 2
+	goto $+1				; 2 These are the two extra cycles above
+	movlw D'164'			; 1
+	movwf delayCounter	  ; 1
 
 delay1msLoop
-    decfsz delayCounter, f  ; 1*delayCounter
-    goto delay1msLoop       ; 2*delayCounter
-    return                  ; 2
+	decfsz delayCounter, f  ; 1*delayCounter
+	goto delay1msLoop	   ; 2*delayCounter
+	return				  ; 2
 
 delay3ms
-    call delay1ms
-    call delay1ms
-    call delay1ms
-    return
+	call delay1ms
+	call delay1ms
+	call delay1ms
+	return
 
-    end
+	end
